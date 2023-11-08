5ffd0f268c40e68294d5355e5f0a36f93a0366b8
thatoneguylearning <142750216+thatoneguylearning@users.noreply.github.com>
2023-11-08 17:22:21 +0000

    Pending changes exported from your codespace

--- bot.py
+++ bot.py
@@ -10,14 +10,11 @@ from io import BytesIO
 from tensorflow.keras.models import load_model
 import aiohttp
 import discord
-from discord import Button, ButtonStyle, ActionRow
 from collections import deque
 
-TOKEN = os.environ["token"]
-channel_ids = [1155093664339599361, 1155093652981420085, 1155093632471285792, 1155093621381545994, 1155093610371481711, 1155093590725365853, 1155093590725365853, 1155093058539495444,1155651042688651264] 
-ownerid = int(os.environ["user_id"])
+TOKEN = "MTA3MzY0MjM1OTI4MTE3MjU1MA.Gmcz8u.kufTWnMDMAQJeu5f5izEuIafuomx8iuGJWh3xE"
+server_id = 1169676280640315544
 poketwo = 716390085896962058
-captchachannel = 1154374370647625799
 bot_prefix = "."
 bot = commands.Bot(command_prefix=bot_prefix, self_bot=True)
 loaded_model = load_model('model.h5', compile=False)
@@ -31,15 +28,6 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.offline)
     print('We have logged in as {0.user}'.format(bot))
 
-@bot.command()
-async def buy_incense(ctx):
-    for channel_id in channel_ids:
-        channel = bot.get_channel(channel_id)
-        if channel:
-            await channel.send('<@716390085896962058> buy incense')
-            await asyncio.sleep(2)
-    await ctx.send("I bought incense in all channels.")
-
 async def process_responses():
     while True:
         if response_queue:
@@ -52,7 +40,7 @@ async def process_responses():
 
 @bot.event
 async def on_message(message):
-    if message.channel.id in channel_ids:
+    if message.guild.id == server_id:
         if len(message.embeds) > 0:
             embed = message.embeds[0]
             if "appeared!" in embed.title:
@@ -74,32 +62,10 @@ async def on_message(message):
                         async with message.channel.typing():
                             await asyncio.sleep(0.3)
 
-                        await message.channel.send(f'<@716390085896962058> catch {name.lower()}')
+                        await message.channel.send(f'<@{poketwo}> catch {name.lower()}')
 
                     response_queue.append(send_response)
 
-        elif message.content.startswith('----join'):
-            invite_link = message.content[6:]
-            try:
-                invite = await bot.fetch_invite(invite_link)
-                await invite.accept()
-                await message.channel.send('Successfully joined the server!')
-            except discord.errors.NotFound:
-                await message.channel.send('The invite link is invalid.')
-        elif 'human' in message.content:
-            print('Captcha Detected')
-            member = message.guild.get_member(poketwo)
-            if member:
-                await member.remove_roles(discord.PermissionOverwrite(administrator=False))
-                await member.kick()
-
-                pattern = r'https://verify\.poketwo\.net/captcha/[0-9]+'
-                match = re.search(pattern, message.content)
-                if match:
-                    url = match.group()
-                    channel = bot.get_channel(captchachannel)
-                    await asyncio.sleep(2)
-                    await channel.send(f"<@{ownerid}> Please verify the poketwo captcha asap! {url}")
         else:
             await bot.process_commands(message)
 
